39654ebfda35d58e1838c8d947e8e0ebb9b0f7e7
Cole-Devv <cole.nelms.02@gmail.com>
2024-01-02 18:13:40 -0500

    added rustup

--- configuration.nix
+++ configuration.nix
@@ -83,12 +83,16 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+  #  wget  
+    pkgs.gcc
+    pkgs.rustup
+
     pkgs.git
     pkgs.helix
     pkgs.bottom
     pkgs.zip
     pkgs.unzip
+
     pkgs.feh
     pkgs.alacritty
     pkgs.firefox
