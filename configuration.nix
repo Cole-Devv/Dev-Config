25369f82b1fb678f71ec5a337bb7405f27a56721
Cole-Devv <cole.nelms.02@gmail.com>
2024-01-05 21:13:12 -0500

    remove dev packages

--- configuration.nix
+++ configuration.nix
@@ -84,10 +84,6 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget  
-    pkgs.gcc
-    pkgs.rustup
-    pkgs.wasmtime
-
     pkgs.git
     pkgs.helix
     pkgs.bottom
