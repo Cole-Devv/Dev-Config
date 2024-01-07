416efbef3ce18a7fc6c4e6d2eafd70e7dc5130a5
Cole-Devv <cole.nelms.02@gmail.com>
2024-01-07 17:07:33 -0500

    added rust and updated rebuild command

--- configuration.nix
+++ configuration.nix
@@ -83,7 +83,10 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget  
+  #  wget
+    pkgs.gcc
+    pkgs.rustup
+
     pkgs.git
     pkgs.helix
     pkgs.bottom
